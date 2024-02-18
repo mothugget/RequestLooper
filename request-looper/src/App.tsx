1deab49209c763665b770a1a8262e6f1625a3488
Karl Fredriksson <karlfredriksson@Karls-MacBook-Air.local>
2024-02-18 08:49:47 +0100

    created app

---
+++ request-looper/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(): ReactElement|null {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
