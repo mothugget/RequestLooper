ba5c1abef67902d73639c7aea715640538a6713e
Karl Fredriksson <karlfredriksson@Karls-MacBook-Air.local>
2024-03-01 08:03:30 +0100

    hard coded test fetch string

--- request-looper/src/contexts/RequestContext.tsx
+++ request-looper/src/contexts/RequestContext.tsx
@@ -4,12 +4,11 @@ import { FetchRequest } from "../types/types";
 interface RequestContextType {
   fetchRequest: FetchRequest;
   setFetchRequest: Dispatch<SetStateAction<FetchRequest>>;
-
 }
 
-const emptyFetchRequest:FetchRequest={
-  variableIndicator:'||',
-  fetchString: '',
+const emptyFetchRequest: FetchRequest = {
+  variableIndicator: "||",
+  fetchString:'http://localhost:3001/,{\nmethod: "POST",\nheaders: {\n    "Content-Type": "application/json",\n    "x-rdp-version": "8.1",\n    "x-rdp-clientId": "rdpclient",\n    "x-rdp-userId": "system",\n    "auth-client-id": AUTHID,\n    "auth-client-secret": CLISEC,\n},\nbody: JSON.stringify({\n    "params": {\n        "query": {\n            "filters": {\n                "typesCriterion": [\n                    "tradegrade"\n                ]\n            },\n            "id": "a4a02416-dbcf-4141-b353-3b7d8e172577"\n        },\n        "fields": {\n            "attributes": [\n                "_ALL"\n            ]\n        },\n        "options": {\n            "maxRecords": 100\n        }\n    }\n}\n)\n}',
   showOptions: {
     resource: false,
     method: false,
@@ -24,24 +23,27 @@ const emptyFetchRequest:FetchRequest={
     integrity: false,
     keepalive: false,
     signal: false,
-    priority: false
-  }
-}
+    priority: false,
+  },
+};
 
-const dummyFetchRequestSetter = 'dummy' as unknown as Dispatch<SetStateAction<FetchRequest>>
+const dummyFetchRequestSetter = "dummy" as unknown as Dispatch<
+  SetStateAction<FetchRequest>
+>;
 
 const RequestContext = createContext<RequestContextType>({
-  fetchRequest: emptyFetchRequest ,
+  fetchRequest: emptyFetchRequest,
   setFetchRequest: dummyFetchRequestSetter,
 });
 function RequestContextProvider({ children }: any) {
-  const [fetchRequest, setFetchRequest] = useState<FetchRequest>(emptyFetchRequest);
- 
+  const [fetchRequest, setFetchRequest] =
+    useState<FetchRequest>(emptyFetchRequest);
+
   return (
     <RequestContext.Provider
       value={{
-fetchRequest,
-setFetchRequest
+        fetchRequest,
+        setFetchRequest,
       }}
     >
       {children}
